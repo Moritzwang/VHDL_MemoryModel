71f9ef4d83d12ee2157ef4c7203da48833f802e1
cvetoslav <ceco552@gmail.com>
2024-11-03 11:55:37 +0100

    initial

---
+++ MemoryModel.srcs/sources_1/new/BitTypedMemory.vhd
@@ -0,0 +1,38 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/03/2024 11:36:02 AM
+-- Design Name: 
+-- Module Name: BitTypedMemory - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity RAM4096x12 is
+    port(
+        w_en    : in bit;
+        addr    : in bit_vector( 11 downto 0 );
+        data_in : in bit_vector( 11 downto 0 );
+        data_out: out bit_vector( 11 downto 0 )
+    );
+end RAM4096x12;
+
+architecture RAM4096x12_BitTyped of RAM4096x12 is
+begin
+
+end RAM4096x12_BitTyped;
---
+++ MemoryModel.srcs/sources_1/new/TestBench.vhd
@@ -0,0 +1,28 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/03/2024 11:36:02 AM
+-- Design Name: 
+-- Module Name: BitTypedMemory - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity TestBench is
+
+end TestBench;
