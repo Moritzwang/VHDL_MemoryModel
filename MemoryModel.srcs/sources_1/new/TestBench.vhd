714f2ac57ef68639a32f9a2ae9f1492b9c87fe85
Moritzwang <129863504+Moritzwang@users.noreply.github.com>
2024-11-06 21:30:51 +0100

    TestBench.vhd

--- MemoryModel.srcs/sources_1/new/TestBench.vhd
+++ MemoryModel.srcs/sources_1/new/TestBench.vhd
@@ -35,16 +35,46 @@ entity TestBench is
 end TestBench;
 
 architecture TB_TEST of TestBench is
+    -- DUT 端口信号声明
+    signal w_en        : bit;
+    signal addr        : bit_vector(11 downto 0);
+    signal dataToMem   : bit_vector(11 downto 0);
+    signal dataFromMem : bit_vector(11 downto 0);
+
+    -- 组件声明（DUT 实体声明）
+    component RAM4096x12
+        port(
+            w_en    : in bit;
+            addr    : in bit_vector(11 downto 0);
+            data_in : in bit_vector(11 downto 0);
+            data_out: out bit_vector(11 downto 0)
+        );
+    end component;
+
 begin
-    
+    -- 实例化 DUT 并连接端口
+    UUT: RAM4096x12
+        port map(
+            w_en => w_en,
+            addr => addr,
+            data_in => dataToMem,
+            data_out => dataFromMem
+        );
+
     process
+        variable i : integer := 0;
     begin
-    
-        for i in 0 to 2**12-1 loop
-            addr <= to_bitvector(std_logic_vector(to_unsigned(i, 12)));
-            dataToMem <= to_bitvector(std_logic_vector(to_unsigned(i, 12)));
-            w_en <= '1'; wait for 1ns;
-            w_en <= '0'; wait for 1ns;
+        w_en <= '1';
+        for i in 0 to 4095 loop
+            addr <= std_logic_vector(to_unsigned(i, 12));
+            dataToMem <= std_logic_vector(to_unsigned(i, 12));
+            wait for 10 ns;
+        end loop;
+
+        w_en <= '0';
+        for i in 0 to 4095 loop
+            addr <= std_logic_vector(to_unsigned(i, 12));
+            wait for 10 ns;
         end loop;
         
         wait;
